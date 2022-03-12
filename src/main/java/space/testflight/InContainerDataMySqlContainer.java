eee41897bbbc9e346d4e5467fced9ddade942608
arne <arne@apache.org>
2022-03-12 10:27:37 +0100

    Fix Testcontainers regression
    (See https://github.com/testcontainers/testcontainers-java/issues/4667)

--- src/main/java/space/testflight/InContainerDataMySqlContainer.java
+++ src/main/java/space/testflight/InContainerDataMySqlContainer.java
@@ -20,6 +20,10 @@ import org.testcontainers.containers.InternetProtocol;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.images.builder.ImageFromDockerfile;
 
+import com.github.dockerjava.api.model.ExposedPort;
+import com.github.dockerjava.api.model.Ports;
+import com.github.dockerjava.api.model.Ports.Binding;
+
 public class InContainerDataMySqlContainer extends MySQLContainer<InContainerDataMySqlContainer>
   implements DefaultTaggableContainer<InContainerDataMySqlContainer> {
 
@@ -30,11 +34,8 @@ public class InContainerDataMySqlContainer extends MySQLContainer<InContainerDat
       .from(image)
       .run("mkdir -p /var/lib/mysql-data && chown -R mysql:mysql /var/lib/mysql-data")
       .build()));
-    initCommand();
-  }
-
-  private void initCommand() {
     withClasspathResourceMapping("/space/testflight/mysql/mysql.cnf", "/etc/mysql/conf.d/mysql.cnf", BindMode.READ_ONLY);
+    exposeContainerPort();
   }
 
   @Override
@@ -50,5 +51,40 @@ public class InContainerDataMySqlContainer extends MySQLContainer<InContainerDat
   @Override
   public void addFixedPort(int hostPort, int containerPort) {
     super.addFixedExposedPort(hostPort, containerPort, InternetProtocol.TCP);
+  }
+
+  private void exposeContainerPort() {
+    withCreateContainerCmdModifier(cmd -> {
+//      List<ExposedPort> exposedPorts = new ArrayList<>();
+//      ExposedPort containerPort = null;
+//      for (ExposedPort p : cmd.getExposedPorts()) {
+//        exposedPorts.add(p);
+//        if (p.getPort() == getContainerPort()) {
+//          containerPort = p;
+//        }
+//      }
+//      if (containerPort == null) {
+//        containerPort = ExposedPort.tcp(getContainerPort());
+//        exposedPorts.add(containerPort);
+//      }
+//      cmd.withExposedPorts(exposedPorts);
+//
+//      Ports ports = cmd.getHostConfig().getPortBindings();
+//      if (!ports.getBindings().containsKey(containerPort)) {
+//        ports.bind(ExposedPort.tcp(getContainerPort()), Ports.Binding.empty());
+//        cmd.getHostConfig().withPortBindings(ports);
+//      }
+//      Ports ports = cmd.getHostConfig().getPortBindings();
+//      ExposedPort exposedContainerPort = ExposedPort.tcp(getContainerPort());
+//      Binding[] bindings = ports.getBindings().get(exposedContainerPort);
+//      String hostIp = null;
+//      for (Binding binding: bindings) {
+//        hostIp = binding.getHostIp();
+//      }
+//      if (hostIp == null) {
+//        ports.bind(exposedContainerPort, Ports.Binding.empty());
+//        cmd.getHostConfig().withPortBindings(ports);
+//      }
+    });
   }
 }
